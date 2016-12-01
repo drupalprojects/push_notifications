1bca603641b41454fd0334fb8d151400a52adb1b
signalpoint <signalpoint@150680.no-reply.drupal.org>
2016-12-01 17:25:51 -0500

    Issue #2761401 by tyler.frankenstein: Firebase Cloud Messaging Support

--- scripts/push_notifications_admin.js
+++ scripts/push_notifications_admin.js
@@ -1,14 +1,23 @@
 (function ($) {
   $(document).ready(function(){
-    // Only show options for either C2DM or GCM.
+    // Only show options for C2DM, GCM or FCM.
   	$('input#edit-push-notifications-google-type-0').click(function(e) {
       $('fieldset#edit-c2dm-credentials').show();
       $('fieldset#edit-gcm-credentials').hide();
+      $('fieldset#edit-fcm-credentials').hide();
   	});
     
   	$('input#edit-push-notifications-google-type-1').click(function(e) {
       $('fieldset#edit-c2dm-credentials').hide();
-      $('fieldset#edit-gcm-credentials').show();      
-  	});    
+      $('fieldset#edit-gcm-credentials').show();
+      $('fieldset#edit-fcm-credentials').hide();
+    });
+
+    $('input#edit-push-notifications-google-type-2').click(function(e) {
+      $('fieldset#edit-c2dm-credentials').hide();
+      $('fieldset#edit-gcm-credentials').hide();
+      $('fieldset#edit-fcm-credentials').show();
+    });
+
   });  
 })(jQuery);
